181b42c480ee60efda1df603187831d4a6c2c02a
Hagos Beyene <53543538+Hagos29@users.noreply.github.com>
2025-01-19 16:18:01 +0300

    Quiz project modified

--- quiz.js
+++ quiz.js
@@ -3,13 +3,11 @@ function checkAnswer() {
     const correctAnswer=4;
     const userAnswer=document.querySelector('input[name="quiz"]:checked').value;
     
-    if (userAnswer===correctAnswer) {
-        document.querySelector('#feedback').textContent='Correct! Well Done';
+    if (userAnswer === correctAnswer) {
+        document.querySelector('#feedback').textContent='Correct! Well done.';
     }else{
         document.querySelector('#feedback').textContent="That's incorrect. Try again!";
 }
 }
-const answer=document.querySelector('#submit-answer');
-answer.addEventListener('click', function(e){
-    checkAnswer
-})
+const submitAnswer=document.querySelector('#submit-answer');
+submitAnswer.addEventListener('click',checkAnswer)
